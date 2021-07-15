3d1c0bc1c6b1cb2eb68d61f4ff4de94b794308af
vryadav <vryadav@arista.com>
2021-07-15 14:48:14 -0700

    adding palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,37 @@
+'''
+
+'''
+
+def palindromeUsingList(s):
+   ps = s[::-1]
+   if ps == s:
+      return True
+   else:
+      return False
+
+def palindromeUsingBuiltInFn(s):
+   ps = ''.join(reversed(s))
+   if ps == s:
+      return True
+   else:
+      return False
+   return
+
+def palindromeUsingForLoop(s):
+   ps=''
+   ln =len(s)
+   for i in range(ln):
+      ps += s[ln -1 - i]
+   if ps == s:
+      return True
+   else:
+      return False
+   return
+
+s = "rekha"
+print("Is '%s' a palindrome" %s)
+
+print("Using List: %s "%palindromeUsingList(s))
+print("Using Built in fn: %s "%palindromeUsingBuiltInFn(s))
+
+print("Using for loop: %s "%palindromeUsingForLoop(s))
